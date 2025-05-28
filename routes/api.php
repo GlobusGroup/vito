e9f07c432b8371f5519a4aaf832e4c09e063a35c
Gab <gabriele.soldati@globusgroup.com>
2025-05-28 16:32:09 +0100

    Add throttling

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Api\SecretController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/secrets/{secret}', [SecretController::class, 'decrypt'])->name('secrets.decrypt');
+Route::post('/secrets/{secret}', [SecretController::class, 'decrypt'])->name('secrets.decrypt')->middleware('throttle:20,1');
